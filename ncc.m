b196be4ec39b5521abe0d06ce2e8306003d6876d
mye-LJH <2193361360@qq.com>
2025-03-26 13:38:14 +0800

    Add files via upload

--- ncc.m
+++ ncc.m
@@ -1,7 +1,7 @@
 function nccValue = ncc(watermark1, watermark2)
     % NCC 计算两个水印之间的归一化互相关
-    watermark1 = double(watermark1);
-    watermark2 = double(watermark2);
+    watermark1 = double(watermark1);  % 转换为双精度
+    watermark2 = double(watermark2);  % 转换为双精度
     mean1 = mean(watermark1(:));  % 水印1的均值
     mean2 = mean(watermark2(:));  % 水印2的均值
     numerator = sum((watermark1(:) - mean1) .* (watermark2(:) - mean2));  % 计算分子
